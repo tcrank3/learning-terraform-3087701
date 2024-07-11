8964e74b4496679ee6d9b3c72bcbb0ca9ddc143e
tcrank3 <tcrank3@yahoo.com>
2024-07-10 20:55:43 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,31 +33,31 @@ module "blog_vpc" {
   }
 }
 
-#resource "aws_instance" "blog" {
-#  ami                    = data.aws_ami.app_ami.id
-#  instance_type = var.instance_type
-#  vpc_security_group_ids = [module.blog_sg.security_group_id]
-#  subnet_id = module.blog_vpc.public_subnets[0]
-#  tags = {
-#    Name = "HelloWorld"
-#  }
-#}
-
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
-
-  name     = "blog"
-  min_size = 1
-  max_size = 2
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_group      = [module.blog_sg.security_group_id]
-
-  image_id      = data.aws_ami.app_ami.id
+resource "aws_instance" "blog" {
+  ami                    = data.aws_ami.app_ami.id
   instance_type = var.instance_type
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id = module.blog_vpc.public_subnets[0]
+  tags = {
+    Name = "HelloWorld"
+  }
 }
+
+#module "autoscaling" {
+#  source  = "terraform-aws-modules/autoscaling/aws"
+#  version = "6.5.2"
+
+#  name     = "blog"
+#  min_size = 1
+#  max_size = 2
+
+#  vpc_zone_identifier = module.blog_vpc.public_subnets
+#  target_group_arns   = module.blog_alb.target_group_arns
+#  security_group      = [module.blog_sg.security_group_id]
+
+#  image_id      = data.aws_ami.app_ami.id
+#  instance_type = var.instance_type
+#}
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
